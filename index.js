6341527f9dc8bf485a05690b802f308d176be663
Jérémie Zarca <jeremie@Macbookair.local>
2017-09-16 19:25:43 +0200

    bugfix

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ const program = require('commander');
 const Api = require('./lib/noon-pacific-api');
 const api = new Api();
 
-const Spotify = require('./lib/spotify');
-const Deezer = require('./lib/deezer');
-
 program
   .version('0.1.0')
-  .option('-l, --list', 'Retrieve all the mixtapes available')
+  .option('-l, --list <type>', 'Retrieve all the mixtapes available [los-angeles|new-york|london|singles]')
   .option('-m, --mixtape <number>', 'Retrieve the tracks in selected mixtape')
   .option('-p --provider <type>', 'Select music provider [spotify]')
   .parse(process.argv);
 
 if (program.list) {
-    return api.getMixtapes()
+    return api.getMixtapes(program.list)
     .then(response => console.log(response));
 }
 
@@ -29,14 +26,16 @@ if (program.mixtape) {
     .then(results => {
         switch(program.provider) {
             case 'spotify':
-                new Spotify(`NOON // ${program.mixtape}`, results.map(e => e.stream_url), false);
+                const Spotify = require('./lib/spotify');
+                new Spotify(program.mixtape, results.map(e => e.stream_url), false);
                 break;
             case 'deezer':
-                new Deezer(`NOON // ${program.mixtape}`, results, false);
+                const Deezer = require('./lib/deezer');            
+                new Deezer(program.mixtape, results, false);
                 break;
             default:
                 console.log("no default provider");
-                new Spotify(`NOON // ${program.mixtape}`, results.map(e => e.stream_url), false);
+                new Spotify(program.mixtape, results.map(e => e.stream_url), false);
                 break;  
         }
     })
